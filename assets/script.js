2ba8ec18448c9ec2bf5e610c4e7a68ddd79b98ca
mjkonkel <mitchell.konkel@gmail.com>
2022-09-29 22:49:04 -0500

    added button icon, fixed hour display

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,28 @@
 var todayDate = moment().format('dddd, MMMM Do');
 $('#currentDay').text(todayDate);
 console.log(todayDate)
+var currentHour = moment().hour();
+console.log(currentHour)
 
-var hours = ['9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00']
 
+var hours = [9,10,11,12,13,14,15,16,17]
 
 function createRow() {
-    
 
     for (var i = 0; i < hours.length; i++) {
-        $('.container').append($('<div><p></p><textarea></textarea><button></button></div>'));
-        $('.container').children().addClass('row');
-        $('.container').children('div').children('p').addClass('hour').text(hours[i]);
-        $('.container').children('div').children('textarea');
-        $('.container').children('div').children('button').addClass('saveBtn');
+        $('.container').append($('<div><p>'+hours[i]+':00</p><textarea></textarea><button><i></i></button></div>'));
+        console.log(hours[i]);
     }
+    $('.container').children('div').children('p').addClass('hour');
+    $('.container').children().addClass('row');
+    $('.container').children('div').children('textarea');
+    $('.container').children('div').children('button').addClass('saveBtn');
+    $('.container').children('div').children('button').children('i').addClass('fas fa-save');
 
+    if (currentHour === hours[i]){
+    $('.container').children('div').children('textarea').addClass('present');
 
+    }
 }
 
 
